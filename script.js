c11cbc85c4525f085a57b8e28bfd2fac2a35ede7
Janani303 <84324472+Janani303@users.noreply.github.com>
2021-05-28 15:55:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-/* Number Guessing Game JavaScript */
+let guessBox = document.getElementById("number-input");
+let check = document.getElementById("guess");
+let history = document.getElementById("history");
+const randomNumber = Math.floor(Math.random()*100+1);
+let count = 1;
+let finalResult = false;
+history.innerHTML = "";
 
-var secretNumber = 0,
-	numberOfGuesses = 0;
+function GuessingGame() {
+    if (finalResult === false) {
+        computeWinner();
+        viewHistory();
+        return finalResult;
+    }
+}   
 
-function writeMessage(elementId, message, appendMessage) {
-	var elemToUpdate = document.getElementById(elementId);
-	if (appendMessage) {
-		elemToUpdate.innerHTML = elemToUpdate.innerHTML + message;
-	} else {
-		elemToUpdate.innerHTML = message;
-	}
-};
+function viewHistory() {
+    let manualGuess = guessBox.value;
+    history.innerHTML+=". your guessed🤔🤔value= " + manualGuess;
 
-function newGame() {
-	secretNumber = Math.floor(Math.random() * 100) + 1;
-	numberOfGuesses = 0;
-	writeMessage('historyList', '');
 }
 
-function guessInRange(guess) {
-	return (guess > 0 && guess < 101);
+function computeWinner() {
+    let result = document.getElementById("result");
+    let guess = guessBox.value;
+
+    if (count < 5) {
+        if (randomNumber == guess) {
+            result.innerHTML ="<h2>super!💥🤩🎊you reached🤝🥳!</h2>";
+            finalResult = true;
+        } else if (randomNumber < guess) {
+            result.innerHTML = "<h2>You guessed too high!😲</h2>";
+            finalResult = false;
+        } else {
+            result.innerHTML = "<h2>You guessed too Low!🙄</h2>";
+            finalResult = false;
+        }
+        count ++;
+    } else {
+        if (randomNumber == guess) {
+            result.innerHTML = "<h2>wow!🤩you reached👍🥳🤝</h2>";
+            finalResult = true;
+        }
+        else {
+            result.innerHTML = "<h2>oops!😔you failed👎</h2>";
+            finalResult = true;
+        }
+    }
 }
 
-function userGuessed() {
-	var userGuessed = document.getElementById('userGuess').value;
-	var statusArea = document.getElementById('statusArea');
-	var historyList = document.getElementById('historyList');
-	if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
-		// Nothing entered or our of range.
-		writeMessage('statusArea', '<p>Please enter a number 1-100 and press the Guess button.</p>');
-	} else if (userGuessed.indexOf('.') != -1) {
-		writeMessage('statusArea', '<p>Please enter a whole number 1-100 and press the Guess button.</p>');
-	} else {
-		numberOfGuesses++;
 
-		if (userGuessed == secretNumber) {
-			// Got it
-			writeMessage('statusArea', '<p>You got me in ' + numberOfGuesses +' guesses, I was thinking ' + secretNumber + '. Let\'s go again...</p>');
-			newGame();
-		} else if (userGuessed < secretNumber) {
-			// User needs to guess higher
-			writeMessage('statusArea', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
-			writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
-		} else {
-			// User needs to guess lower
-			writeMessage('statusArea', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
-			writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
-		}
-	}
 
-	document.getElementById('userGuess').value = '';	
-}
-
-window.onload = function() {
-	newGame();
-	document.getElementById('buttonArea').addEventListener('click', userGuessed);
-};
+check.addEventListener("click", GuessingGame);
